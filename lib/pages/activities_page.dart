a21ae35fa8a08f619459f4b8bf55f4b03c963678
mr-jw <joshua.walley01@gmail.com>
2023-03-22 17:29:07 +0000

    Integrated Calendar

--- lib/pages/activities_page.dart
+++ lib/pages/activities_page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:fitness_tracker/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:fitness_tracker/widgets/activity_card_widget.dart';
@@ -5,9 +7,14 @@ import 'package:fitness_tracker/model/activity.dart';
 import 'package:fitness_tracker/db/activity_database.dart';
 import 'package:fitness_tracker/pages/edit_activity_page.dart';
 import 'package:fitness_tracker/pages/activity_detail_page.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'analytics_page.dart';
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
+}
 
 class ActivityPage extends StatefulWidget {
   const ActivityPage({Key? key}) : super(key: key);
@@ -18,8 +25,11 @@ class ActivityPage extends StatefulWidget {
 
 class _ActivityPageState extends State<ActivityPage> {
   String _appBarTitle = "Activities";
+
   // database communication variables.
   late List<Activity> _activities;
+  int i = 0;
+
   bool _isLoading = false;
 
   // page naivgation variables.
@@ -44,11 +54,30 @@ class _ActivityPageState extends State<ActivityPage> {
     });
   }
 
+  // table calendar variables/attributes.
+  final CalendarFormat _calendarFormat = CalendarFormat.week;
+  final RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.disabled;
+  DateTime _focusedDay = DateTime.now();
+  late DateTime _selectedDay = _focusedDay;
+
+// debug code.
+  Future addActivity(int day) async {
+    final activity = Activity(
+      title: "Test Activity",
+      description: "Test Description.",
+      date: DateTime.utc(2023, 03, day),
+      audio: "...",
+      mood: 1.5,
+    );
+
+    await ActivityDatabase.instance.create(activity);
+  }
+
   @override
   void initState() {
     super.initState();
 
-    refreshActivities();
+    _getEventsForDay();
   }
 
   @override
@@ -57,20 +86,92 @@ class _ActivityPageState extends State<ActivityPage> {
     ActivityDatabase.instance.close();
   }
 
-  Future refreshActivities() async {
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+
+      _getEventsForDay();
+    }
+  }
+
+  void _getEventsForDay() async {
     setState(() => _isLoading = true);
 
     _activities = await ActivityDatabase.instance.readAllActivities();
 
     setState(() => _isLoading = false);
+
+    removeInvalidActivities();
+  }
+
+  void removeInvalidActivities() {
+    i = 0;
+    while (i < _activities.length) {
+      if (isSameDay(_activities[i].date, _selectedDay)) {
+        i++;
+      } else {
+        // date doesn't match, remove it.
+        _activities.remove(_activities[i]);
+
+        if (i == _activities.length) {
+          break;
+        } else {
+          i = 0;
+          continue;
+        }
+      }
+    }
+  }
+
+  Widget buildCalendar() {
+    return TableCalendar(
+      focusedDay: _focusedDay,
+      firstDay: DateTime.utc(2023),
+      lastDay: DateTime.utc(2035),
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+      calendarFormat: _calendarFormat,
+      rangeSelectionMode: _rangeSelectionMode,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      headerStyle: const HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+      ),
+      calendarStyle: CalendarStyle(
+        outsideDaysVisible: false,
+        todayTextStyle: const TextStyle(
+          color: Colors.black,
+        ),
+        // Use `CalendarStyle` to customize the UI
+        todayDecoration: BoxDecoration(
+          color: Colors.green.shade200,
+          shape: BoxShape.circle,
+        ),
+        // Use `CalendarStyle` to customize the UI
+        selectedDecoration: BoxDecoration(
+          color: Colors.green.shade400,
+          shape: BoxShape.circle,
+        ),
+        selectedTextStyle: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      onDaySelected: _onDaySelected,
+      onPageChanged: (focusedDay) {
+        _focusedDay = focusedDay;
+      },
+    );
   }
 
   Widget activitiesPageContent() {
     return Column(
       children: [
+        buildCalendar(),
         Expanded(
           child: _isLoading
-              ? const CircularProgressIndicator()
+              ? const Center(child: CircularProgressIndicator())
               : _activities.isEmpty
                   ? const Center(
                       child: Text(
@@ -99,7 +200,7 @@ class _ActivityPageState extends State<ActivityPage> {
                     ActivityDetailPage(activityId: activity.id!),
               ));
 
-              refreshActivities();
+              _getEventsForDay();
             },
             child: ActivityCardWidget(activity: activity, index: index),
           );
@@ -125,7 +226,7 @@ class _ActivityPageState extends State<ActivityPage> {
                   builder: (context) => const AddEditActivityPage()),
             );
 
-            refreshActivities();
+            _getEventsForDay();
           },
         ),
         bottomNavigationBar: BottomNavigationBar(
